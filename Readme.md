cb28d92aab3beed05f44d2228494f3dc6b393dcc
K Balaji Naik <101827986+Balaji91221@users.noreply.github.com>
2023-06-28 17:02:05 +0530

    first commit

---
+++ Readme.md
@@ -0,0 +1,42 @@
+# Discord Bot
+
+This is a simple Discord bot that responds to user messages with various commands.
+
+## Features
+
+- **Hello**: Send the bot a message saying "hello" to receive a friendly greeting.
+- **Roll**: Type "roll" to get a random number between 1 and 6.
+- **Help**: Send the bot the command "!help" to get assistance and information about available commands.
+- **Custom Responses**: The bot also provides custom responses to certain inputs, such as "hi."
+
+## Usage
+
+1. Invite the bot to your Discord server.
+2. Start a conversation with the bot by mentioning its name or sending a direct message.
+3. Use the supported commands mentioned above to interact with the bot.
+
+Feel free to modify the `handle_response` function in the `bot.py` script to add more custom responses based on your requirements.
+
+## Requirements
+
+- Python 3.7 or higher
+- discord.py library
+
+## Installation
+
+1. Clone the repository to your local machine.
+2. Install the required dependencies by running the following command:
+
+
+pip install discord.py
+
+3. Replace the `'YOUR_DISCORD_TOKEN'` placeholder in `bot.py` with your actual Discord bot token.
+4. Run the bot script by executing the following command:
+
+
+python bot.py
+
+
+Make sure you have created a Discord bot and obtained a valid bot token before running the script. For more information on creating a Discord bot and obtaining a token, refer to the Discord Developer Portal documentation.
+
+Feel free to explore and modify the code according to your needs. Enjoy using the Discord bot!
---
+++ bot.py
@@ -0,0 +1,80 @@
+import discord
+import random
+
+# Function to handle user messages and generate responses
+def handle_response(message) -> str:
+    p_message = message.lower()
+    if p_message == 'hello':
+        return 'Hi there!'
+    elif p_message == 'roll':
+        return str(random.randint(1, 6))
+    elif p_message == '!help':
+        return "This is a help message that you can modify."
+    elif p_message == 'hi':
+        return 'How can I help you?'
+        
+    else:
+        return "Yeah, I don't know. Try typing '!help'."
+
+# Send messages
+async def send_message(message, user_message, is_private):
+    try:
+        response = handle_response(user_message)
+        
+        if is_private:
+            await message.author.send(response)
+        else:
+            await message.channel.send(response)
+        
+        # Send the response to the mobile device
+        # Replace the following line with the code to send the response to your mobile device
+        send_to_mobile(response)
+        
+    except Exception as e:
+        print(e)
+
+# Main function to run the Discord bot
+def run_discord_bot():
+    TOKEN = 'MTEyMzUxNTczODM2NDMxNzc3Nw.G5XuhW.GExZ1VUos6lXgTQyUhL3LZRQgdXbmGtfYimrYg'
+    intents = discord.Intents.default()
+    intents.typing = False
+
+    client = discord.Client(intents=intents)
+
+    @client.event
+    async def on_ready():
+        print(f'{client.user} is now running!')
+
+    @client.event
+    async def on_message(message):
+        # Make sure bot doesn't get stuck in an infinite loop
+        if message.author == client.user:
+            return
+
+        # Get data about the user
+        username = str(message.author)
+        user_message = str(message.content)
+        channel = str(message.channel)
+
+        # Debug printing
+        print(f"{username} said: '{user_message}' ({channel})")
+
+        # If the user message contains a '?' in front of the text, it becomes a private message
+        if user_message[0] == '?':
+            user_message = user_message[1:]  # [1:] Removes the '?'
+            await send_message(message, user_message, is_private=True)
+        else:
+            await send_message(message, user_message, is_private=False)
+
+    # Run the bot with your Discord token
+    client.run(TOKEN)
+
+# Function to send response to your mobile device
+def send_to_mobile(response):
+    # Add your code to send the response to your mobile device
+    # This can be using push notifications, messaging services, or any other method of your choice
+    # Replace this placeholder function with your actual implementation
+    print("Sending response to mobile:", response)
+
+# Run the Discord bot
+run_discord_bot()
